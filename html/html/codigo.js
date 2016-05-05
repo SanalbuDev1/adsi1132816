0ca3366f6f325fa0a259eacf6a9226296bf1c314
ofac <ofaczero@gmail.com>
2016-05-05 12:32:12 -0500

    Enlaces / Rutas

---
+++ html/html/codigo.js
@@ -0,0 +1,11 @@
+
+var nombre = prompt('Cuál es su nombre?');
+
+if (nombre == 'oliver') 
+{
+	alert('Bienvenido: '+nombre+' el goleador');
+} 
+else 
+{
+	alert('Bienvenido: '+nombre+' el defensa');
+}
